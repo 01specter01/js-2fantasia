560e73d65c6757cf3d2126ce99260f83409936b8
Leo Volwahsen <volwahsenl@gmail.com>
2022-07-06 14:53:01 +0200

    arrays alles was man wissen sollte

---
+++ index.js
@@ -0,0 +1,7 @@
+//1 Ein Array ist ein Datensatz wie eine Variable aber im Gegenteil zu vatiabele kann das arry deutlich mehr Daten beinhalten.
+
+//2 arrays braucht man um serverdaten zu erfassen und zu bearbeiten
+
+//3 Arrays sind auch oft effectiver und schneller zu schreiben
+
+//4 Arrays haben auch den vorteil das man nich wissen muss was die daten genau beinhalzten um diese'zu (löschen/hinzufügen/überspringen/teilen/zusammenfügen)
